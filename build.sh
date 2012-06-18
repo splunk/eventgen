9dfff156039e707c32e8feb4d3ca1ff9b78ed102
Clint Sharp <clint@typhoon.org>
2012-06-18 13:05:30 -0600

    Added tutorial files and updated README

--- build.sh
+++ build.sh
@@ -8,12 +8,13 @@ CURRENT_PWD=`pwd`
 rm $CURRENT_PWD/eventgen.spl
 
 # Create a build directory that we can use
-BUILD_DIR=.build/SA-eventgen
+BUILD_DIR=.build/SA-Eventgen
 BUILD_DIR_PARENT=.build
 mkdir -p $BUILD_DIR
 
 cp -R * $BUILD_DIR
 cd $BUILD_DIR_PARENT
+rm SA-Eventgen/local/eventgen-standalone.conf
 tar cfz $CURRENT_PWD/eventgen.spl SA-Eventgen --exclude "SA-Eventgen/eventgen.spl" --exclude "SA-Eventgen/.*"
 cd $CURRENT_PWD
 rm -rf $BUILD_DIR
