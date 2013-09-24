b2998965bea92b09b7b00d80cf7e09cf244693c8
Clint Sharp <clint@typhoon.org>
2013-09-24 05:41:43 -0700

    Merging large CSV reading change from Hemenedra

--- lib/eventgensamples.py
+++ lib/eventgensamples.py
@@ -182,6 +182,8 @@ class Sample:
                 logger.debug("Reading csv sample '%s' in app '%s'" % (self.name, self.app))
                 sampleDict = [ ]
                 sampleLines = [ ]
+                # Fix to load large csv files, work with python 2.5 onwards
+                csv.field_size_limit(sys.maxint)
                 csvReader = csv.DictReader(sampleFH)
                 for line in csvReader:
                     sampleDict.append(line)
