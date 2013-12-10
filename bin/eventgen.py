5d9c930a2cb07a48120a94616e96c8aa81ad04e2
Clint Sharp <clint@typhoon.org>
2013-12-09 23:21:41 -0800

    Working implementation of modular inputs

--- bin/eventgen.py
+++ bin/eventgen.py
@@ -14,108 +14,7 @@ import time
 import datetime
 from select import select
 from eventgenconfig import Config
-
-class Timer(threading.Thread):
-# class Timer(multiprocessing.Process):
-    time = None
-    stopping = None
-    interruptcatcher = None
-    countdown = None
-    
-    # Added by CS 5/7/12 to emulate threading.Timer
-    def __init__(self, time, sample=None, interruptcatcher=None):
-        self.time = time
-        self.stopping = False
-        self.interruptcatcher = interruptcatcher
-        self.countdown = 0
-        
-        self.sample = sample
-        threading.Thread.__init__(self)
-        # multiprocessing.Process.__init__(self)
-
-    def run(self):
-        if self.sample.delay > 0:
-            logger.info("Sample set to delay %s, sleeping." % s.delay)
-            time.sleep(self.sample.delay)
-        while (1):
-            if not self.stopping:
-                if not self.interruptcatcher:
-                    if self.countdown <= 0:
-                        try:
-                            partialInterval = self.sample.gen()
-                        # 11/24/13 CS Blanket catch for any errors
-                        # If we've gotten here, all error correction has failed and we
-                        # need to gracefully exit providing some error context like what sample
-                        # we came from
-                        except (KeyboardInterrupt, SystemExit):
-                            raise
-                        except:
-                            import traceback
-                            logger.error('Exception in sample: %s\n%s' % (self.sample.name, \
-                                    traceback.format_exc()))
-                            sys.stderr.write('Exception in sample: %s\n%s' % (self.sample.name, \
-                                    traceback.format_exc()))
-                            sys.exit(1)
-
-                        self.countdown = partialInterval
-
-                        ## Sleep for partial interval
-                        # If we're going to sleep for longer than the default check for kill interval
-                        # go ahead and flush output so we're not just waiting
-                        if partialInterval > self.time:
-                            logger.debugv("Flushing because we're sleeping longer than a polling interval")
-                            self.sample._out.flush()
-
-                            # Make sure that we're sleeping an accurate amount of time, including the
-                            # partial seconds.  After the first sleep, we'll sleep in increments of
-                            # self.time to make sure we're checking for kill signals.
-                            sleepTime = self.time + (partialInterval % self.time)
-                            self.countdown -= sleepTime
-                        else:
-                            sleepTime = partialInterval
-                            self.countdown = 0
-                          
-                        logger.debug("Generation of sample '%s' in app '%s' sleeping for %f seconds" \
-                                    % (self.sample.name, self.sample.app, partialInterval) ) 
-                        logger.debug("Queue depth for sample '%s' in app '%s': %d" % (self.sample.name, self.sample.app, c.outputQueue.qsize()))   
-                        if sleepTime > 0:
-                            self.sample.saveState()
-                            time.sleep(sleepTime)
-                    else:
-                        self.countdown -= self.time
-                        time.sleep(self.time)
-                else:
-                    time.sleep(self.time)
-            else:
-                sys.exit(0)
-
-    def stop(self):
-        self.sample.saveState()
-        self.stopping = True
-                     
-            
-# Copied from http://danielkaes.wordpress.com/2009/06/04/how-to-catch-kill-events-with-python/
-def set_exit_handler(func):
-	if os.name == "nt":
-		try:
-			import win32api
-			win32api.SetConsoleCtrlHandler(func, True)
-		except ImportError:
-			version = ".".join(map(str, sys.version_info[:2]))
-			raise Exception("pywin32 not installed for Python " + version)
-	else:
-		import signal
-		signal.signal(signal.SIGTERM, func)
-		signal.signal(signal.SIGINT, func)
-    
-def handle_exit(sig=None, func=None):
-    print '\n\nCaught kill, exiting...'
-    for sampleTimer in sampleTimers:
-        sampleTimer.stop()
-    for worker in workers:
-        worker.stop()
-    sys.exit(0)
-    		
+from eventgentimer import Timer
 
 if __name__ == '__main__':
     debug = False
@@ -143,9 +42,6 @@ if __name__ == '__main__':
         
     c.parse()
 
-    sampleTimers = []
-    workers = []
-
     if c.runOnce:
         logger.info('Entering debug (single iteration) mode')
 
@@ -160,26 +56,20 @@ if __name__ == '__main__':
             else:
                 logger.info("Creating timer object for sample '%s' in app '%s'" % (s.name, s.app) )    
                 t = Timer(1.0, s) 
-                sampleTimers.append(t)
+                c.sampleTimers.append(t)
     
     ## Start the timers
     if not c.runOnce:
         if os.name != "nt":
-            set_exit_handler(handle_exit)
+            c.set_exit_handler(c.handle_exit)
         first = True
         while (1):
             try:
                 ## Only need to start timers once
                 if first:
-                    logger.info('Starting timers')
-                    for sampleTimer in sampleTimers:
-                        sampleTimer.start()
-                    for x in xrange(0, c.outputWorkers):
-                        worker = c.getPlugin('OutputWorker')()
-                        worker.start()
-                        workers.append(worker)
+                    c.start()
                     first = False
                 logger.info('Queue depth: %d' % c.outputQueue.qsize())
                 time.sleep(5)
             except KeyboardInterrupt:
-                handle_exit()
+                c.handle_exit()
---
+++ lib/eventgentimer.py
@@ -0,0 +1,91 @@
+import threading
+import time
+import logging
+from eventgenconfig import Config
+import sys
+
+class Timer(threading.Thread):
+# class Timer(multiprocessing.Process):
+    time = None
+    stopping = None
+    interruptcatcher = None
+    countdown = None
+    
+    # Added by CS 5/7/12 to emulate threading.Timer
+    def __init__(self, time, sample=None, interruptcatcher=None):
+        # Logger already setup by config, just get an instance
+        logger = logging.getLogger('eventgen')
+        globals()['logger'] = logger
+
+        globals()['c'] = Config()
+
+        self.time = time
+        self.stopping = False
+        self.interruptcatcher = interruptcatcher
+        self.countdown = 0
+        
+        self.sample = sample
+        threading.Thread.__init__(self)
+        # multiprocessing.Process.__init__(self)
+
+    def run(self):
+        if self.sample.delay > 0:
+            logger.info("Sample set to delay %s, sleeping." % s.delay)
+            time.sleep(self.sample.delay)
+        while (1):
+            if not self.stopping:
+                if not self.interruptcatcher:
+                    if self.countdown <= 0:
+                        try:
+                            partialInterval = self.sample.gen()
+                        # 11/24/13 CS Blanket catch for any errors
+                        # If we've gotten here, all error correction has failed and we
+                        # need to gracefully exit providing some error context like what sample
+                        # we came from
+                        except (KeyboardInterrupt, SystemExit):
+                            raise
+                        except:
+                            import traceback
+                            logger.error('Exception in sample: %s\n%s' % (self.sample.name, \
+                                    traceback.format_exc()))
+                            sys.stderr.write('Exception in sample: %s\n%s' % (self.sample.name, \
+                                    traceback.format_exc()))
+                            sys.exit(1)
+
+                        self.countdown = partialInterval
+
+                        ## Sleep for partial interval
+                        # If we're going to sleep for longer than the default check for kill interval
+                        # go ahead and flush output so we're not just waiting
+                        if partialInterval > self.time:
+                            logger.debugv("Flushing because we're sleeping longer than a polling interval")
+                            self.sample._out.flush()
+
+                            # Make sure that we're sleeping an accurate amount of time, including the
+                            # partial seconds.  After the first sleep, we'll sleep in increments of
+                            # self.time to make sure we're checking for kill signals.
+                            sleepTime = self.time + (partialInterval % self.time)
+                            self.countdown -= sleepTime
+                        else:
+                            sleepTime = partialInterval
+                            self.countdown = 0
+                          
+                        logger.debug("Generation of sample '%s' in app '%s' sleeping for %f seconds" \
+                                    % (self.sample.name, self.sample.app, partialInterval) ) 
+                        logger.debug("Queue depth for sample '%s' in app '%s': %d" % (self.sample.name, self.sample.app, c.outputQueue.qsize()))   
+                        if sleepTime > 0:
+                            self.sample.saveState()
+                            time.sleep(sleepTime)
+                    else:
+                        self.countdown -= self.time
+                        time.sleep(self.time)
+                else:
+                    time.sleep(self.time)
+            else:
+                sys.exit(0)
+
+    def stop(self):
+        self.sample.saveState()
+        self.stopping = True
+                     
+    		
