af9595510e2d268996e94ed6dff2203876652537
Clint Sharp <clint@typhoon.org>
2014-02-11 13:59:38 -0800

    Adding docstrings

--- lib/eventgenqueue.py
+++ lib/eventgenqueue.py
@@ -8,6 +8,9 @@ import json
 import threading
 
 class Queue:
+    """
+    Abstraction of threading or multiprocessing Queue
+    """
     def __init__(self, depth, threading):
         logger = logging.getLogger('eventgen')
         globals()['logger'] = logger
@@ -23,9 +26,15 @@ class Queue:
         self.depth = depth
 
     def put(self, item, block, timeout):
+        """
+        Put item in queue, with block and timeout passed through
+        """
         self.q.put(item, block, timeout)
 
     def get(self, block, timeout):
+        """
+        Get an item from the queue, with block and timeout passed through
+        """
         return self.q.get(block, timeout)
 
 
