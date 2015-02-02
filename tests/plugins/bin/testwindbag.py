40c0f910b959bc2b03f124e803b4c15d4ae85272
Clint Sharp <clint@typhoon.org>
2015-02-02 10:02:51 -0800

    Cleaning up testwindbag.py

--- tests/plugins/bin/testwindbag.py
+++ tests/plugins/bin/testwindbag.py
@@ -24,32 +24,8 @@ class WindbagGenerator(GeneratorPlugin):
         if self._sample.out == None:
             self.logger.info("Setting up Output class for sample '%s' in app '%s'" % (self._sample.name, self._sample.app))
             self._sample.out = Output(self._sample)
-        # l = range(count)
-        # for i in xrange(count):
-        #     # self._sample.timestamp = latest
-        #     # self._sample.out.send("%s WINDBAG Event %d of %d" % (datetime.datetime.strftime(latest, "%Y-%m-%d %H:%M:%S"), i+1, count))
-        #     self._sample.out.send('2014-01-05 23:07:08 WINDBAG Event 1 of 100000')
-        # i = itertools.imap(lambda x: self._sample.out.send('2014-01-05 23:07:08 WINDBAG Event 1 of 100000'), l)
-        # filter(lambda x: x, i)
-        # l = [ ]
-        # for i in xrange(count):
-        #     # l.append({ '_raw': "%s WINDBAG Event %d of %d" % (datetime.datetime.strftime(latest, "%Y-%m-%d %H:%M:%S"), i+1, count),
-        #     # l.append({'_raw': '2014-01-05 23:07:08 WINDBAG Event 1 of 100000', 
-        #     #             'index': self._sample.index,
-        #     #             'host': self._sample.host,
-        #     #             'source': self._sample.source,
-        #     #             'sourcetype': self._sample.sourcetype,
-        #     #             '_time': latest})
-        #     l.append({'_raw': '2014-01-05 23:07:08 WINDBAG Event 1 of 100000'})
+        
         l = [ {'_raw': '2014-01-05 23:07:08 WINDBAG Event 1 of 100000'} for i in xrange(count) ]
-        # l = [ {'_raw': '2014-01-05 23:07:08 WINDBAG Event 1 of 100000', 
-        #                 'index': self._sample.index,
-        #                 'host': self._sample.host,
-        #                 'source': self._sample.source,
-        #                 'sourcetype': self._sample.sourcetype,
-        #                 '_time': time.mktime(latest.timetuple()) } for i in xrange(count) ]
-        # l = [ {'_raw': '"111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111"'} for i in xrange(count) ]
-        # l = [ '2014-01-05 23:07:08 WINDBAG Event 1 of 100000' for i in xrange(count) ]
 
         self._sample.out.bulksend(l)
         return 0
