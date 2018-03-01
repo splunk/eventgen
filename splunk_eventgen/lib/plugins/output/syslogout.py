d8101096cc5fdfceb83e55047f6a6eda6d19ce26
Adam Lamar <alamar@splunk.com>
2018-03-01 11:18:02 -0700

    syslogout handler added once per logger

--- splunk_eventgen/lib/plugins/output/syslogout.py
+++ splunk_eventgen/lib/plugins/output/syslogout.py
@@ -2,6 +2,9 @@ from __future__ import division
 from outputplugin import OutputPlugin
 import logging, logging.handlers
 
+# Dict of flags to gate adding the syslogHandler only once to the given singleton logger
+loggerInitialized = {}
+
 class SyslogOutOutputPlugin(OutputPlugin):
     useOutputQueue = True
     name = 'syslogout'
@@ -15,10 +18,17 @@ class SyslogOutOutputPlugin(OutputPlugin):
         self._syslogDestinationHost = sample.syslogDestinationHost if hasattr(sample, 'syslogDestinationHost') and sample.syslogDestinationHost else '127.0.0.1'
         self._syslogDestinationPort = sample.syslogDestinationPort if hasattr(sample, 'syslogDestinationPort') and sample.syslogDestinationPort else 1514
 
-        self._l = logging.getLogger('syslog'+ sample.name)
+        loggerName = 'syslog' + sample.name
+        self._l = logging.getLogger(loggerName)
         self._l.setLevel(logging.INFO)
-        syslogHandler = logging.handlers.SysLogHandler(address=(self._syslogDestinationHost, int(self._syslogDestinationPort)))
-        self._l.addHandler(syslogHandler)
+
+        global loggerInitialized
+        # This class is instantiated at least once each interval. Since each logger with a given name is a singleton,
+        # only add the syslog handler once instead of every interval.
+        if not loggerName in loggerInitialized:
+            syslogHandler = logging.handlers.SysLogHandler(address=(self._syslogDestinationHost, int(self._syslogDestinationPort)))
+            self._l.addHandler(syslogHandler)
+            loggerInitialized[loggerName] = True
 
     def flush(self, q):
         for x in q:
