98eb65ecd942b33d208362a5b7ee5c7bc0c89cb1
jackm <jackm@splunk.com>
2018-11-26 13:41:21 -0800

    Add safety check for verbosity / Fix token parsing error

--- splunk_eventgen/lib/eventgenconfig.py
+++ splunk_eventgen/lib/eventgenconfig.py
@@ -311,7 +311,8 @@ class Config(object):
                 # Get the latest token number of the current stanza
                 last_token_number = 0
                 for key, value in settings.items():
-                    if 'token' in key and int(key[6]) > last_token_number:
+                    self.logger.info("key: {0}\nvalue: {1}".format(key, value))
+                    if 'token' in key and key[6].isdigit() and int(key[6]) > last_token_number:
                         last_token_number = int(key[6])
 
                 # Apply global tokens to the current stanza
