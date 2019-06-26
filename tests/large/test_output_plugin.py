61b56741a5711dfc2189e1a97fb65821e82f9dbc
Lynch Wu <lwu@splunk.com>
2019-06-27 07:00:57 +0800

    add functional tests for devnull/file/httpevent

---
+++ tests/large/test_output_plugin.py
@@ -0,0 +1,46 @@
+import os
+import subprocess
+import time
+from utils.splunk_search_util import get_session_key, preprocess_search, run_search, get_search_response
+
+
+def test_plugin_devnull(eventgen_test_helper):
+    """Test output plugin devnull"""
+    events = eventgen_test_helper("eventgen_plugin_devnull.conf").get_events()
+    # assert the events size is 0
+    assert len(events) == 0
+
+
+def test_plugin_file(eventgen_test_helper):
+    """Test output plugin file"""
+    events = eventgen_test_helper("eventgen_plugin_file.conf").get_events()
+    # assert the events size is 12 when end = 1
+    assert len(events) == 12
+
+
+def test_plugin_httpevent(eventgen_test_helper):
+    """Test output plugin httpevent"""
+    base_dir = os.path.dirname(os.path.abspath(__file__))
+    docker_compose_file = os.path.join(base_dir, "provision/docker-compose.yml")
+    up_cmd = ["docker-compose", "-f", docker_compose_file, "up"]
+    subprocess.Popen(up_cmd, stdout=subprocess.PIPE)
+
+    time.sleep(30)
+    # https://github.com/docker/compose/issues/5696
+    provision_cmd = ["docker-compose -f " + docker_compose_file +
+                     " exec -T splunk sh -c 'cd /opt/splunk;./provision.sh;/opt/splunk/bin/splunk restart'"]
+
+    try:
+        subprocess.check_output(provision_cmd, shell=True, stderr=subprocess.STDOUT)
+    except subprocess.CalledProcessError as e:
+        raise RuntimeError("command '{}' return with error (code {}): {}".format(e.cmd, e.returncode, e.output))
+
+    time.sleep(30)
+    eventgen_test_helper("eventgen_plugin_httpevent.conf").get_events()
+
+    session_key = get_session_key()
+    search_job_id = run_search(session_key, preprocess_search('index=main'))
+    events = get_search_response(session_key, search_job_id)
+    assert len(events) == 12
+    down_cmd = ["docker-compose", "-f", docker_compose_file, "down"]
+    subprocess.Popen(down_cmd, stdout=subprocess.PIPE)
---
+++ tests/large/utils/splunk_search_util.py
@@ -0,0 +1,62 @@
+import urllib
+import httplib2
+from xml.dom import minidom
+import time
+import json
+
+BASEURL = 'https://localhost:8089'
+USERNAME = 'admin'
+PASSWORD = 'changeme'
+
+
+def get_session_key():
+    server_content = httplib2.Http(disable_ssl_certificate_validation=True).request(
+        BASEURL + '/services/auth/login',
+        'POST',
+        headers={},
+        body=urllib.urlencode({'username': USERNAME, 'password': PASSWORD})
+    )[1]
+    try:
+        session_key = minidom.parseString(server_content).getElementsByTagName('sessionKey')[0].childNodes[0].nodeValue
+        return session_key
+    except Exception as e:
+        raise Exception('Failed to get session key: %s' % str(e))
+
+
+def preprocess_search(search_query):
+    # Remove leading and trailing whitespace from the search
+    search_query = search_query.strip()
+    # If the query doesn't already start with the 'search' operator or another
+    # generating command (e.g. "| inputcsv"), then prepend "search " to it.
+    if not (search_query.startswith('search') or search_query.startswith('|')):
+        search_query = 'search ' + search_query
+    return search_query
+
+
+def run_search(session_key, search_query):
+    response = httplib2.Http(disable_ssl_certificate_validation=True).request(
+        BASEURL + '/services/search/jobs',
+        'POST',
+        headers={'Authorization': 'Splunk %s' % session_key},
+        body=urllib.urlencode({'search': search_query})
+    )[1]
+    # return search job id
+    try:
+        return minidom.parseString(response).getElementsByTagName('sid')[0].childNodes[0].nodeValue
+    except Exception as e:
+        raise Exception('Failed to start search: %s' % str(e))
+
+
+def get_search_response(session_key, search_job_id):
+    # wait one second for the search to complete
+    time.sleep(1)
+    results = httplib2.Http(disable_ssl_certificate_validation=True).request(
+        BASEURL + '/services/search/jobs/%s/results' % search_job_id,
+        'GET',
+        headers={'Authorization': 'Splunk %s' % session_key},
+        body=urllib.urlencode({'output_mode': 'json'})
+    )
+    try:
+        return json.loads(results[1])['results']
+    except Exception as e:
+        raise Exception('Failed to get search results: %s' % str(e))
