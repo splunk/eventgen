84e73682ca3354b6bd223332c7d5e2bc1be25811
tonyl <tonyl@splunk.com>
2019-07-01 13:53:06 -0700

    Hello world

--- splunk_eventgen/eventgen_api_server/eventgen_server.py
+++ splunk_eventgen/eventgen_api_server/eventgen_server.py
@@ -1,17 +1,22 @@
 from flask import Flask
 
-registered_routes = {}
-def register_route(route):
-    def inner(fn):
-        registered_routes[route] = fn
-        return fn
-    return inner
-
-class EventgenServer(Object):
+class EventgenServer():
 
     def __init__(self, *args, **kwargs):
         self.app = self._create_app()
 
+    def app_run(self):
+        self.app.run(host="0.0.0.0", port=9500)
+    
     def _create_app(self):
         app = Flask(__name__)
         app.config['SECRET_KEY'] = 'does-not-exist'
+        
+        # register api endpoints
+        # app.register_blueprint(CreateWorkflowApi(self.logger).get_blueprint())
+        # app.register_blueprint(OtherApi(self.logger).get_blueprint())
+        @app.route('/')
+        def index():
+            return "helloworld"
+            
+        return app
