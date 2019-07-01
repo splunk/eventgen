10ab0b8ec4d9eeec6317d9780ff82ea24fb2ca48
tonyl <tonyl@splunk.com>
2019-07-01 14:05:15 -0700

    Initial core object

---
+++ splunk_eventgen/eventgen_api_server/eventgen_core_object.py
@@ -0,0 +1,46 @@
+import argparse
+from argparse import Namespace
+import logging
+import os
+import sys
+
+import splunk_eventgen.eventgen_core as eventgen_core
+
+class EventgenCoreObject():
+    def __init__(self):
+        self.logger = logging.getLogger('eventgen_server')
+        self.eventgen_core_object = eventgen_core.EventGenerator(self._create_args())
+        self.configured = False
+        self.configfile = None
+    
+    def refresh_eventgen_core_object(self):
+        self.eventgen_core_object = eventgen_core.EventGenerator(self._create_args())
+        self.configured = False
+        self.configfile = 'N/A'
+        self.logger.info("Refreshed the eventgen core object")
+
+    def _create_args(self):
+        args = Namespace()
+        args.daemon = False
+        args.verbosity = None
+        args.version = False
+        args.backfill = None
+        args.count = None
+        args.devnull = False
+        args.disableOutputQueue = False
+        args.end = None
+        args.generators = None
+        args.interval = None
+        args.keepoutput = False
+        args.modinput = False
+        args.multiprocess = False
+        args.outputters = None
+        args.profiler = False
+        args.sample = None
+        args.version = False
+        args.subcommand = 'generate'
+        args.verbosity = 20
+        args.wsgi = True
+        args.modinput_mode = False
+        return args
+
--- splunk_eventgen/eventgen_api_server/eventgen_server.py
+++ splunk_eventgen/eventgen_api_server/eventgen_server.py
@@ -1,9 +1,11 @@
 from flask import Flask
+import eventgen_core_object
 
 class EventgenServer():
 
     def __init__(self, *args, **kwargs):
         self.app = self._create_app()
+        self.eventgen = eventgen_core_object.EventgenCoreObject() 
 
     def app_run(self):
         self.app.run(host="0.0.0.0", port=9500)
@@ -11,10 +13,10 @@ class EventgenServer():
     def _create_app(self):
         app = Flask(__name__)
         app.config['SECRET_KEY'] = 'does-not-exist'
-        
-        # register api endpoints
+
         # app.register_blueprint(CreateWorkflowApi(self.logger).get_blueprint())
         # app.register_blueprint(OtherApi(self.logger).get_blueprint())
+
         @app.route('/')
         def index():
             return "helloworld"
