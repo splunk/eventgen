56186f605641919b19c39e79984e1320edf8a8ea
tonyl <tonyl@splunk.com>
2019-07-09 10:13:38 -0700

    Dockerfile fix for stdout

--- dockerfiles/entrypoint.sh
+++ dockerfiles/entrypoint.sh
@@ -5,14 +5,11 @@ set -e
 if [ "$#" = 0 ]; then
 	tail -F -n0 /etc/hosts && wait
 elif [ "$1" = "controller" ]; then
-	splunk_eventgen service --role controller &
-	tail -F -n0 /etc/hosts && wait
+	splunk_eventgen service --role controller
 elif [ "$1" = "server" ]; then
-	splunk_eventgen service --role server &
-	tail -F -n0 /etc/hosts && wait
+	splunk_eventgen service --role server
 elif [ "$1" = "standalone" ]; then
-	splunk_eventgen service --role standalone &
-	tail -F -n0 /etc/hosts && wait
+	splunk_eventgen service --role standalone
 else
 	"$@"
 fi
