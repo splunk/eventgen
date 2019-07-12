f489cc2260a7647a795f405c9848d0bfb1112174
Li Wu <wuli.rye@gmail.com>
2019-07-12 09:41:44 +0800

    add ft for splunkstream and spool (#268)

--- tests/large/test_output_plugin.py
+++ tests/large/test_output_plugin.py
@@ -1,6 +1,3 @@
-import os
-import subprocess
-import time
 from utils.splunk_search_util import get_session_key, preprocess_search, run_search, get_search_response
 
 
@@ -29,8 +26,24 @@ def test_plugin_httpevent(eventgen_test_helper):
 
 
 def test_plugin_s2s(eventgen_test_helper):
+    """Test output plugin s2s"""
     eventgen_test_helper("eventgen_plugin_s2s.conf").get_events()
     session_key = get_session_key()
     search_job_id = run_search(session_key, preprocess_search('index=main sourcetype=s2s'))
     events = get_search_response(session_key, search_job_id)
+    assert len(events) == 12
+
+
+def test_plugin_splunkstream(eventgen_test_helper):
+    """Test output plugin splunkstream"""
+    eventgen_test_helper("eventgen_plugin_splunkstream.conf", env={"PYTHONHTTPSVERIFY": "0"}).get_events()
+    session_key = get_session_key()
+    search_job_id = run_search(session_key, preprocess_search('index=main sourcetype=splunkstream'))
+    events = get_search_response(session_key, search_job_id)
+    assert len(events) == 12
+
+
+def test_plugin_spool(eventgen_test_helper):
+    """Test output plugin spool"""
+    events = eventgen_test_helper("eventgen_plugin_spool.conf").get_events()
     assert len(events) == 12
