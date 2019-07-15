f7a0bac109a44d83a8f63716f9b339cb80caa51d
Lynch Wu <lwu@splunk.com>
2019-07-15 16:16:44 +0800

    Add test case for issue #269

---
+++ tests/large/test_extend_index.py
@@ -0,0 +1,14 @@
+from utils.splunk_search_util import get_session_key, preprocess_search, run_search, get_search_response
+
+
+def test_extend_index(eventgen_test_helper):
+    """Test extendIndexes config"""
+    eventgen_test_helper("eventgen_extend_index.conf").get_events()
+
+    session_key = get_session_key()
+    search_job_id = run_search(session_key, preprocess_search('index=main sourcetype=cisco'))
+    test_index_search_job_id = run_search(session_key, preprocess_search('index=test_*'))
+    main_events = get_search_response(session_key, search_job_id)
+    test_index_events = get_search_response(session_key, test_index_search_job_id)
+    assert len(main_events) == 12
+    assert len(test_index_events) == 12
