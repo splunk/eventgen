a2d774ee4d1f8bd915b6ca0a7e9560491ccd481f
tonyl <tonyl@splunk.com>
2019-07-15 20:08:22 -0700

    Introducing tail

--- dockerfiles/entrypoint.sh
+++ dockerfiles/entrypoint.sh
@@ -7,11 +7,14 @@ if [ "$#" = 0 ]; then
 	tail -F -n0 /etc/hosts && wait
 elif [ "$1" = "controller" ]; then
 	redis-server &
-	splunk_eventgen service --role controller
+	splunk_eventgen service --role controller &
+	tail -F -n0 /etc/hosts && wait
 elif [ "$1" = "server" ]; then
-	splunk_eventgen service --role server
+	splunk_eventgen service --role server &
+	tail -F -n0 /etc/hosts && wait
 elif [ "$1" = "standalone" ]; then
-	splunk_eventgen service --role standalone
+	splunk_eventgen service --role standalone &
+	tail -F -n0 /etc/hosts && wait
 else
 	"$@"
 fi
