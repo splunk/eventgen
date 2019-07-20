dd0d01485189c10e0847089fa4e77eb2c2c58057
Ryan Faircloth <rfaircloth@splunk.com>
2019-07-20 13:32:56 -0400

    update entry

--- dockerfiles/entrypoint.sh
+++ dockerfiles/entrypoint.sh
@@ -1,20 +1,16 @@
 #!/bin/bash
 
 set -e
-/usr/sbin/sshd
 
 if [ "$#" = 0 ]; then
 	tail -F -n0 /etc/hosts && wait
 elif [ "$1" = "controller" ]; then
 	redis-server &
-	splunk_eventgen service --role controller &
-	tail -F -n0 /etc/hosts && wait
+	splunk_eventgen service --role controller
 elif [ "$1" = "server" ]; then
-	splunk_eventgen service --role server &
-	tail -F -n0 /etc/hosts && wait
+	splunk_eventgen service --role server
 elif [ "$1" = "standalone" ]; then
-	splunk_eventgen service --role standalone &
-	tail -F -n0 /etc/hosts && wait
+	splunk_eventgen service --role standalone
 else
 	"$@"
 fi
