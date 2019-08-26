fef4a42b634dc3ea02aa8d9d0c53f21430b35a7c
Jack Meixensperger <jackm@splunk.com>
2019-08-26 12:39:04 -0700

    Address comment

--- splunk_eventgen/eventgen_api_server/eventgen_server_api.py
+++ splunk_eventgen/eventgen_api_server/eventgen_server_api.py
@@ -371,7 +371,7 @@ class EventgenServerAPI():
                 stanza_num -= 1
             divided_volume = float(target_volume) / stanza_num
             for stanza, kv_pair in conf_dict.iteritems():
-                if isinstance(kv_pair, dict) and 'global' not in stanza and '.*' not in stanza:
+                if isinstance(kv_pair, dict) and stanza != 'global' and '.*' not in stanza:
                     conf_dict[stanza]["perDayVolume"] = divided_volume
 
         self.set_conf(conf_dict)
