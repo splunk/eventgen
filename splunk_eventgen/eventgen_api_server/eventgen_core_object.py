3888b8d7192762c978552351fdce9e7f7c23a2b6
Lynch Wu <lwu@splunk.com>
2019-11-28 16:18:49 +0800

    Add multithread support

--- splunk_eventgen/eventgen_api_server/eventgen_core_object.py
+++ splunk_eventgen/eventgen_api_server/eventgen_core_object.py
@@ -9,9 +9,9 @@ CUSTOM_CONFIG_PATH = os.path.realpath(os.path.join(FILE_PATH, "..", "default", "
 
 
 class EventgenCoreObject:
-    def __init__(self):
+    def __init__(self, **kargs):
         self.logger = logging.getLogger('eventgen_server')
-        self.eventgen_core_object = eventgen_core.EventGenerator(self._create_args())
+        self.eventgen_core_object = eventgen_core.EventGenerator(self._create_args(**kargs))
         self.configured = False
         self.configfile = None
         self.check_and_configure_eventgen()
@@ -30,7 +30,7 @@ class EventgenCoreObject:
         self.check_and_configure_eventgen()
         self.logger.info("Refreshed the eventgen core object")
 
-    def _create_args(self):
+    def _create_args(self, **kargs):
         args = argparse.Namespace()
         args.daemon = False
         args.version = False
@@ -43,7 +43,7 @@ class EventgenCoreObject:
         args.interval = None
         args.keepoutput = False
         args.modinput = False
-        args.multiprocess = True
+        args.multiprocess = False if kargs.get("mutithread") else True
         args.outputters = None
         args.profiler = False
         args.sample = None
