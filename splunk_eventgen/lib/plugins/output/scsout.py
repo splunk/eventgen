1613b17946ac0264c0e263ebf81e2884d9d130d8
Che-Lun Tsao <ctsao@splunk.com>
2020-02-07 10:46:40 -0800

    re-add SCS access token refresh function

--- splunk_eventgen/lib/plugins/output/scsout.py
+++ splunk_eventgen/lib/plugins/output/scsout.py
@@ -29,28 +29,23 @@ class SCSOutputPlugin(OutputPlugin):
     def __init__(self, sample, output_counter=None):
         OutputPlugin.__init__(self, sample, output_counter)
 
-        # self.scsHttpPayloadMax = 150000 # Documentation recommends 20KB to 200KB. Going with 150KB.
+        self.scsHttpPayloadMax = 150000 # Documentation recommends 20KB to 200KB. Going with 150KB.
         self.scsScheme = getattr(self._sample, "scsScheme", "https")
         self.scsHost = getattr(self._sample, "scsHost", "api.scp.splunk.com")
         self.scsIngestEndPoint = getattr(self._sample, "scsIngestEndPoint")
         self.scsAccessToken = getattr(self._sample, "scsAccessToken")
         self.tenant = getattr(self._sample, "scsTenant")
-        self.verify = False if getattr(self._sample, "scsInsecure") == "true" else True
+        self.verify = False if hasattr(self._sample, "scsInsecure") and getattr(self._sample, "scsInsecure") == "true" else True
 
         if not self.scsIngestEndPoint:
             raise NoSCSEndPoint("Please specify your REST endpoint (events | metrics)")
 
         if not self.scsAccessToken:
-            raise NoSCSAccessToken("Please specify your auth access token for the SCP tenant")
-
-        # self.scsClientId = getattr(self._sample, 'scsClientId', '')
-        # self.scsClientSecret = getattr(self._sample, 'scsClientSecret', '')
-        # self.scsRetryNum = int(getattr(self._sample, 'scsRetryNum', 0)) # By default, retry num is 0
+            raise NoSCSAccessToken("Please specify your auth access token")
 
         self.api_url = f'{self.scsScheme}://{self.scsHost}/{self.tenant}/ingest/v1beta2/{self.scsIngestEndPoint}'
 
-    def flush(self, events):
-
+    def _send_batch(self, events):
         data = json.dumps(events)
 
         n_retry = 0
@@ -85,6 +80,29 @@ class SCSOutputPlugin(OutputPlugin):
             finally:
                 logger.debug(f"Successfully sent out {len(events)} {self.scsIngestEndPoint} of {self._sample.name}")
                 break
+
+    def flush(self, events):
+        logger.info(f"Number of events: {len(events)}")
+
+        while events:
+
+            current_size = 0
+            payload = []
+            
+            while events:
+                current_event = events.pop()
+                payload.append(current_event)
+                current_size += len(current_event['body'])
+                if current_size >= self.scsHttpPayloadMax:
+                    logger.info(f"Sending out batch... {len(payload)} events")
+                    self._send_batch(payload)
+                    break
+                elif not events:
+                    logger.info(f"Sending out last batch... {len(payload)} events")
+                    self._send_batch(payload)
+                
+
+        
 
 def load():
     """Returns an instance of the plugin"""
