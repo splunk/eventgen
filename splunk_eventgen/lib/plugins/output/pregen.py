f1d84e7bfe6482b22751970ca301e66cb16d4143
rfaircloth-splunk <rfaircloth@splunk.com>
2020-05-15 15:57:54 -0400

    Json output plugin
    This plugin provides a json output to simplify integration into pytest-splunk-addon

---
+++ splunk_eventgen/lib/plugins/output/pregen.py
@@ -0,0 +1,33 @@
+import sys
+import json
+
+from splunk_eventgen.lib.outputplugin import OutputPlugin
+
+
+class PregenOutputPlugin(OutputPlugin):
+    name = "pregen"
+    MAXQUEUELENGTH = 10
+    useOutputQueue = True
+
+    def __init__(self, sample, output_counter=None):
+        OutputPlugin.__init__(self, sample, output_counter)
+
+    def flush(self, q):
+        out = ""
+        if len(q) > 0:
+            m = q.pop(0)
+            while m:
+                out  = json.dumps(m)
+
+                try:
+                    m = q.pop(0)
+                except IndexError:
+                    m = False
+
+        sys.stdout.write(out)
+        sys.stdout.flush()
+
+
+def load():
+    """Returns an instance of the plugin"""
+    return PregenOutputPlugin
